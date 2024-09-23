48dc95d3fc6d27736180d77164a21ebd6d236dcc
Adil Yergaliyev <adil.yergaliyev@gmail.com>
2024-09-23 07:11:26 +0200

    Changes the certbot script a bit and nginx configurations.

--- scripts/certbot_entrypoint.py
+++ scripts/certbot_entrypoint.py
@@ -14,36 +14,25 @@ if len(sys.argv) != 3:
 domain = sys.argv[1]
 mode = sys.argv[2]
 
-# Paths to the certificate and key files
-BASE_CERT_DIR = f'/etc/letsencrypt/live/{domain}'
-PROD_CERT_DIR = f'{BASE_CERT_DIR}/production'
-STAGING_CERT_DIR = f'{BASE_CERT_DIR}/staging'
-SELF_SIGNED_CERT_DIR = f'{BASE_CERT_DIR}/self-signed'
-CERT_PATH = f'{PROD_CERT_DIR}/fullchain.pem'
-KEY_PATH = f'{PROD_CERT_DIR}/privkey.pem'
-
-def create_cert_dir_structure():
-    os.makedirs(PROD_CERT_DIR, exist_ok=True)
-    os.makedirs(STAGING_CERT_DIR, exist_ok=True)
-    os.makedirs(SELF_SIGNED_CERT_DIR, exist_ok=True)
+CERT_DIR = f'/etc/letsencrypt/live/{domain}'
 
 def generate_self_signed_cert():
     print("Generating self-signed certificate...")
 
-    create_cert_dir_structure()
+    os.makedirs(CERT_DIR, exist_ok=True)
     
     subprocess.run([
         "openssl", "req", "-x509", "-nodes", "-days", "365", 
         "-newkey", "rsa:2048", 
-        "-keyout", f'{SELF_SIGNED_CERT_DIR}/privkey.pem',
-        "-out", f'{SELF_SIGNED_CERT_DIR}/fullchain.pem',
+        "-keyout", f'{CERT_DIR}/privkey.pem',
+        "-out", f'{CERT_DIR}/fullchain.pem',
         "-subj", f"/CN={domain}"
     ], check=True)
 
 def generate_real_cert(is_staging=False):
     print("Generating real certificate with Certbot...")
     
-    create_cert_dir_structure()
+    os.makedirs(CERT_DIR, exist_ok=True)
     
     certbot_cmd = [
         "certbot", "certonly", "--webroot", "-w", "/var/www/certbot", 
@@ -58,25 +47,17 @@ def generate_real_cert(is_staging=False):
 
 def renew_certificates():
     while True:
-        # Check if the certificates exist
-        if not os.path.isfile(CERT_PATH) or not os.path.isfile(KEY_PATH):
-            if mode == "development":
-                generate_self_signed_cert()
-            elif mode == "production":
-                generate_real_cert()
-            elif mode == "staging":
-                generate_real_cert(is_staging=True)
-            else:
-                print("Invalid mode. Use 'production', 'staging' or 'development'.")
-                sys.exit(1)
+        if mode == "development":
+            generate_self_signed_cert()
+        elif mode == "production":
+            generate_real_cert()
+        elif mode == "staging":
+            generate_real_cert(is_staging=True)
         else:
-            try:
-                # Renew the certificates
-                subprocess.run(["certbot", "renew", "--webroot", "-w", "/var/www/certbot"], check=True)
-            except subprocess.CalledProcessError as e:
-                print(f"Error renewing certificates: {e}")
+            print("Invalid mode. Use 'production', 'staging' or 'development'.")
+            sys.exit(1)
         
-        time.sleep(5184000)  # Sleep for 60 days (60 * 24 * 60 * 60 seconds)
+        time.sleep(604800)  # Sleep for 7 days (7 * 24 * 60 * 60 seconds)
 
 def signal_handler(sig, frame):
     print('Exiting...')
