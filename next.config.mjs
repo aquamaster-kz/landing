1b774690248e53a431ebef9c5a4ba4a3c323fb17
Adil Yergaliyev <adil.yergaliyev@gmail.com>
2024-10-13 22:07:04 +0200

    Removes environment variables from automation, add them manually.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  serverRuntimeConfig: {
+    CONTACT_EMAIL: process.env.CONTACT_EMAIL,
+    CONTACT_PASS: process.env.CONTACT_PASS,
+  },
 };
 
 export default nextConfig;
