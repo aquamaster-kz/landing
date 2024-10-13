e1df1ee12d9e2b622dd631bdc127962dfddf3ad1
Adil Yergaliyev <adil.yergaliyev@gmail.com>
2024-10-13 17:46:09 +0200

    Uses secure protocol.

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,8 +13,8 @@ export async function POST(req: Request) {
 
   const transporter = nodemailer.createTransport({
     host: "smtp.mail.me.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
